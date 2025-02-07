99fc80f3f61b576596c71e61d01c8285fdf17b7a
Kernin <jorgezbrito@gmail.com>
2025-02-07 14:01:01 -0600

    guess receives a new array mixing guess and empty string, separated by letter and displays in space separated for the guesses

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -5,20 +5,46 @@ import { range } from '../../utils';
 function Guess({ guessInput = '', guessList, setGuessList }) {
 	const numGuess = NUM_OF_GUESSES_ALLOWED;
 
-	console.log('guess list: ', guessList);
+	const emptyStringArr = [
+		['', '', '', '', ''],
+		['', '', '', '', ''],
+		['', '', '', '', ''],
+		['', '', '', '', ''],
+		['', '', '', '', ''],
+		['', '', '', '', ''],
+	];
+	// console.log('guess list: ', guessList);
 
-	// guessList now has words separated by words
-	// we now have to place those words in spans
+	// 1. separate guess into letters to map over
+	// 2. check if the guessList is empty and map over to show grid
+	// 3. if the guessList has an item map over it and print it on spans (the letters)
+
+	// 4. done... but how do we show the guess AND the grid at the same time
+
+	// OPTION 2
+	// 1. check if the guessList is empty
+	// 2. if guessList is empty show the whole grid
+	// 3. if guessList has an item... push the rest of the items as empty but iterable
+	// 4. display the list, with a guess and empty ones that complete the amount of Game guess
+
+	const arrDifference = numGuess - guessList.length;
+
+	// Thinking of another way to push new array to existing array is just to expand o replace, with a method
+
+	const newArr = [...guessList, ...emptyStringArr];
+
+	for (let i = guessList.length - 1; i >= 0; i--) {
+		newArr.pop();
+	}
 
 	return (
 		<div className='guess-results'>
-			{/* grid of 6 <p> vertical for guess */}
-			{range(numGuess).map((item) => (
+			{/* si la diferencia NO es 6 mostrara el array con los vacios */}
+			{newArr.map((word, index) => (
 				<p className='guess' key={crypto.randomUUID()}>
-					{/* grid for 5 letter horizontal of the guess */}
-					{range(5).map((letter) => (
+					{range(5).map((letter, index) => (
 						<span className='cell' key={crypto.randomUUID()}>
-							{''}
+							{word[index]}
 						</span>
 					))}
 				</p>
