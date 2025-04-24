ef188476e48b01ce0180ef2db2e6883f17a17208
Kernin <jorgezbrito@gmail.com>
2025-04-23 22:48:05 -0600

    words are passed and then separated by letter but before anything is passed y set to empty string by default, then it displays the letters in spans

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 import { range } from '../../utils';
 
-function Guess({ guessWord }) {
+function Guess({ guessWord = '' }) {
 	const separatedGuess = guessWord.toString().split('');
+
+	// console.log(typeof guessWord, guessWord);
 
 	return (
 		<>
-			{range(NUM_OF_GUESSES_ALLOWED).map((guess) => (
+			{range(5).map((guess) => (
 				<span className='cell' key={crypto.randomUUID()}>
 					{separatedGuess[guess]}
 					{/* {console.log('separated', guessWord.toString().split(''))} */}
